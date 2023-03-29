d19a1e6a482043e764ede1c820490bbbdb4db45b
saul <saul@organicdesign.co.nz>
2023-03-30 11:49:13 +1300

    Add pubsub to helia.

--- test/utils/create-helia.js
+++ test/utils/create-helia.js
@@ -4,12 +4,14 @@ import { webSockets } from '@libp2p/websockets'
 import { all } from '@libp2p/websockets/filters'
 import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
+import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 
 export default async () => {
   const libp2p = await createLibp2p({
     transports: [webSockets({ filter: all })],
     connectionEncryption: [noise()],
     streamMuxers: [mplex()],
+    pubsub: gossipsub({ allowPublishToZeroPeers: true }),
     addresses: {
       listen: ['/ip4/0.0.0.0/tcp/0/ws']
     }
