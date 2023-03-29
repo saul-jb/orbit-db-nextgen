17f4617392fd09878d8014bf2f19bcba74344d2b
saul <saul@organicdesign.co.nz>
2023-03-30 11:28:38 +1300

    Add helia node creation util.

---
+++ test/utils/create-helia.js
@@ -0,0 +1,19 @@
+import { createHelia } from 'helia'
+import { createLibp2p } from 'libp2p'
+import { webSockets } from '@libp2p/websockets'
+import { all } from '@libp2p/websockets/filters'
+import { noise } from '@chainsafe/libp2p-noise'
+import { mplex } from '@libp2p/mplex'
+
+export default async () => {
+  const libp2p = await createLibp2p({
+    transports: [webSockets({ filter: all })],
+    connectionEncryption: [noise()],
+    streamMuxers: [mplex()],
+    addresses: {
+      listen: ['/ip4/0.0.0.0/tcp/0/ws']
+    }
+  })
+
+  return await createHelia({ libp2p })
+}
