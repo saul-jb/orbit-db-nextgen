924f638cc4ef2ac2575b5b424db565a1506e7c1b
saul <saul@organicdesign.co.nz>
2023-03-30 11:33:26 +1300

    Refactor connect nodes util to work with helia.

--- test/utils/connect-nodes.js
+++ test/utils/connect-nodes.js
@@ -2,21 +2,11 @@
 
 const defaultFilter = () => true
 
-const connectIpfsNodes = async (ipfs1, ipfs2, options = {
+const connectIpfsNodes = async (helia1, helia2, options = {
   filter: defaultFilter
 }) => {
-  const id1 = await ipfs1.id()
-  const id2 = await ipfs2.id()
-
-  const addresses1 = id1.addresses.filter(options.filter)
-  const addresses2 = id2.addresses.filter(options.filter)
-
-  for (const a2 of addresses2) {
-    await ipfs1.swarm.connect(a2)
-  }
-  for (const a1 of addresses1) {
-    await ipfs2.swarm.connect(a1)
-  }
+  await helia1.libp2p.peerStore.addressBook.set(helia2.libp2p.peerId, helia2.libp2p.getMultiaddrs().filter(options.filter))
+  await helia1.libp2p.dial(helia2.libp2p.peerId)
 }
 
 export default connectIpfsNodes
