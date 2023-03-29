1eeac00782375e1af0c51ec0e751d119d75d2a8c
saul <saul@organicdesign.co.nz>
2023-03-30 11:32:24 +1300

    Refactor IPFS block storage to helia.

--- src/storage/ipfs-block.js
+++ src/storage/ipfs-block.js
@@ -3,28 +3,19 @@ import { base58btc } from 'multiformats/bases/base58'
 
 const defaultTimeout = 30000
 
-const IPFSBlockStorage = async ({ ipfs, timeout, pin } = {}) => {
+const IPFSBlockStorage = async ({ ipfs, pin } = {}) => {
   if (!ipfs) throw new Error('An instance of ipfs is required.')
-
-  timeout = timeout || defaultTimeout
 
   const put = async (hash, data) => {
     const cid = CID.parse(hash, base58btc)
-    await ipfs.block.put(data, {
-      cid: cid.bytes,
-      version: cid.version,
-      format: 'dag-cbor',
-      mhtype: 'sha2-256',
-      pin,
-      timeout
-    })
+    await ipfs.blockstore.put(cid, data)
   }
 
   const del = async (hash) => {}
 
   const get = async (hash) => {
     const cid = CID.parse(hash, base58btc)
-    const block = await ipfs.block.get(cid, { timeout })
+    const block = await ipfs.blockstore.get(cid)
     if (block) {
       return block
     }
